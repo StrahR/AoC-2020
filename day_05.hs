9d379d865ee4a514aa2ab1f47948bfd6d35f130d
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-05 11:22:58 +0100

    formatting day 5

--- day_05.hs
+++ day_05.hs
@@ -7,18 +7,18 @@ main = do
   writeFile "day_05_2.out" $ show $ naloga2 input'
 
 toNum :: String -> Int
-toNum = let
-      aux [] = 0
-      aux (x : xs) = x + 2 * aux xs
-      bitVal 'F' = 0
-      bitVal 'L' = 0
-      bitVal 'B' = 1
-      bitVal 'R' = 1
-   in aux . reverse . map bitVal
+toNum =
+    let aux [] = 0
+        aux (x : xs) = x + 2 * aux xs
+        bitVal 'F' = 0
+        bitVal 'L' = 0
+        bitVal 'B' = 1
+        bitVal 'R' = 1
+     in aux . reverse . map bitVal
 
 naloga1 = maximum . map toNum
 
 naloga2 tickets =
     let tickets'@(h:_) = sort $ map toNum tickets
         [(a, b)]       = filter (\(x, y) -> x - y /= 1) $ zip tickets' (h-1 : tickets')
-    in quot (a + b) 2
+     in quot (a + b) 2
