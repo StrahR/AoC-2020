c63d0976aecc3fd259203b2d5e70ddafc7baabff
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-05 08:48:35 +0100

    day 5 refactored toNum

--- day_05.hs
+++ day_05.hs
@@ -3,20 +3,18 @@ import Data.List
 main = do
   input <- readFile "day_05.in"
   let input' = lines input
-  -- print $ naloga2 input
-  -- naloga2 input
   writeFile "day_05_1.out" $ show $ naloga1 input'
   writeFile "day_05_2.out" $ show $ naloga2 input'
 
 toNum :: String -> Int
-toNum =
-    let
-        aux "" = 0
-        aux ('F': cs) = 0 + 2 * aux cs
-        aux ('L': cs) = 0 + 2 * aux cs
-        aux ('B': cs) = 1 + 2 * aux cs
-        aux ('R': cs) = 1 + 2 * aux cs
-     in aux . reverse
+toNum = let
+      aux [] = 0
+      aux (x : xs) = x + 2 * aux xs
+      bitVal 'F' = 0
+      bitVal 'L' = 0
+      bitVal 'B' = 1
+      bitVal 'R' = 1
+   in aux . reverse . map bitVal
 
 naloga1 = foldr (max . toNum) 0
 
