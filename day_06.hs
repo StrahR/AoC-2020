8bd3f6f3cdb1815e870f456d715a38f5a63447f5
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-06 15:00:59 +0100

    day 6

---
+++ day_06.hs
@@ -0,0 +1,12 @@
+import Data.List
+import Data.List.Split
+
+main = do
+  input <- readFile "day_06.in"
+  let input' = map lines $ splitOn "\n\n" input :: [[String]]
+  writeFile "day_06_1.out" $ show $ naloga1 input'
+  writeFile "day_06_2.out" $ show $ naloga2 input'
+
+naloga1 = sum . map (length . foldl1' union)
+
+naloga2 = sum . map (length . foldl1' intersect)
