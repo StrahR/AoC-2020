e6180c924672ede971c741936b74b302f0f5a12c
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-07 10:24:59 +0100

    refactored day 7

--- day_07.hs
+++ day_07.hs
@@ -1,14 +1,15 @@
 import Control.Applicative
 import Data.Char
 import Data.List
-import qualified Data.Map.Strict as PlainMap (Map, empty, insert, lookup)
-import qualified Data.MultiMap as Map (MultiMap, empty, fromMap, insert, lookup, toMap)
+import qualified Data.Map.Strict as PlainMap (Map, empty, fromList, insert, lookup)
+import qualified Data.MultiMap as Map (MultiMap, empty, fromList, fromMap, insert, lookup, toList, toMap)
 import qualified Data.Set as Set (Set, empty, insert, member)
 import Text.ParserCombinators.ReadP
 
 main = do
   input <- readFile "day_07.in"
   let input' = lines input :: [String]
+  -- print $ map parseRule input'
   writeFile "day_07_1.out" $ show $ naloga1 input'
   writeFile "day_07_2.out" $ show $ naloga2 input'
 
@@ -34,31 +35,29 @@ bagType = do
 
 rule = do
   ruleName <- name
-  string " bags contain "
-  contains <- many1 bagType
+  string " bags contain no other bags." <|> string " bags contain "
+  contains <- option [] (many1 bagType)
   _ <- eof
   return (ruleName, contains)
 
-parseName = readP_to_S name
+parseRule = fst . head . readP_to_S rule
 
-parseBagType = readP_to_S bagType
+-- buildRulesBackwards = foldl (\map (parent, children) -> foldl (\m c -> Map.insert (snd c) parent m) map children) Map.empty
+buildRulesBackwards = Map.fromList . map (\(k, v) -> (snd v, k)) . Map.toList . buildRulesForwards
 
-parseRule = readP_to_S rule
-
-buildRules = foldl (\map (parent, children) -> foldl (\m c -> Map.insert (snd c) parent m) map children) Map.empty
-
-floodFill1 m =
+countParents m =
   let aux visited [] = visited
       aux visited (x : xs) = aux (Set.insert x visited) (xs ++ Map.lookup x m)
-   in aux Set.empty $ Map.lookup "shiny gold" m
+   in length $ aux Set.empty $ Map.lookup "shiny gold" m
 
-naloga1 = length . floodFill1 . buildRules . map (fst . head) . filter (not . null) . map parseRule
+naloga1 = countParents . buildRulesBackwards . map parseRule
 
-buildRules' = Map.fromMap . foldl (\m (parent, children) -> PlainMap.insert parent children m) PlainMap.empty
+-- buildRulesForwards = Map.fromMap . foldl (\m (parent, children) -> PlainMap.insert parent children m) PlainMap.empty
+buildRulesForwards = Map.fromMap . PlainMap.fromList
 
-floodFill2 m =
+countChildren m =
   let aux k [] = k
       aux k ((n, x) : xs) = aux (k + n) (xs ++ concat (replicate n (Map.lookup x m)))
    in aux 0 $ Map.lookup "shiny gold" m
 
-naloga2 = floodFill2 . buildRules' . map (fst . head) . filter (not . null) . map parseRule
+naloga2 = countChildren . buildRulesForwards . map parseRule
