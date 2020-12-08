a58b8ce66fbf4e98dd159ed9886777b1c877ad82
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-08 21:32:01 +0100

    refactor 8

--- day_08.hs
+++ day_08.hs
@@ -1,15 +1,14 @@
-import Data.Char
-import Data.Map.Strict (Map, (!))
-import qualified Data.Map.Strict as Map (fromList, insert, size)
-import Data.Set (Set)
-import qualified Data.Set as Set (empty, insert, member)
-import Text.ParserCombinators.ReadP (eof, many1, readP_to_S, satisfy)
+import           Data.Char
+import           Data.Map.Strict              (Map, (!))
+import qualified Data.Map.Strict              as Map (fromList, insert, size)
+import qualified Data.Set                     as Set (empty, insert, member)
+import           Text.ParserCombinators.ReadP (eof, many1, readP_to_S, satisfy)
 
 main = do
   input <- readFile "day_08.in"
   let input' = loadProgramToMemory $ map (fst . head . parseOp) $ lines input :: Map Int (String, Int)
   let input'' = Map.insert (Map.size input') ("end", 0) input'
-  --   print $ parseOp (input' ! 0)
+  -- print $ parseOp "jmp +333"
   writeFile "day_08_1.out" $ show $ naloga1 input''
   writeFile "day_08_2.out" $ show $ naloga2 input''
 
@@ -28,8 +27,8 @@ opP = do
   space
   pm <- satisfy isAscii
   arg <- number
-  let arg' = if pm == '-' then - read arg :: Int else read arg
-  _ <- eof
+  eof
+  let arg' = if pm == '-' then - read arg else read arg :: Int
   return (optcode, arg')
 
 parseOp = readP_to_S opP
@@ -37,36 +36,27 @@ parseOp = readP_to_S opP
 loadProgramToMemory :: [a] -> Map Int a
 loadProgramToMemory xs = Map.fromList $ zip [0 .. length xs] xs
 
-handheldSim :: Map Int (String, Int) -> (Int, Int)
+handheldSim :: Map Int (String, Int) -> (Bool, Int)
 handheldSim mem =
-  let call pc acc hist
-        | not (Set.member pc hist) =
-          aux optcode arg pc acc (Set.insert pc hist)
-        where
-          (optcode, arg) = mem ! pc
-      call _ acc _ = (1, acc)
-      aux "acc" arg pc acc hist = call (pc + 1) (acc + arg) hist
-      aux "jmp" arg pc acc hist = call (pc + arg) acc hist
-      aux "nop" _ pc acc hist = call (pc + 1) acc hist
-      aux "end" _ _ acc _ = (0, acc)
-   in call 0 0 Set.empty
+  let dispatch ("acc", arg) pc acc hist = exec (pc + 1) (acc + arg) hist
+      dispatch ("jmp", arg) pc acc hist = exec (pc + arg) acc hist
+      dispatch ("nop", _)   pc acc hist = exec (pc + 1) acc hist
+      dispatch ("end", _)   _  acc _    = (True, acc)
+      exec pc acc hist =
+        if not (Set.member pc hist)
+          then dispatch (mem ! pc) pc acc (Set.insert pc hist)
+          else (False, acc)
+   in exec 0 0 Set.empty
 
 naloga1 = snd . handheldSim
 
-handheldSimSub :: [(Int, (String, Int))] -> Map Int (String, Int) -> (Int, Int)
-handheldSimSub [] = handheldSim
+handheldSimSub :: [(Int, (String, Int))] -> Map Int (String, Int) -> (Bool, Int)
+handheldSimSub []              = handheldSim
 handheldSimSub ((pc, op) : xs) = handheldSimSub xs . Map.insert pc op
 
 naloga2 mem =
-  let aux pc =
-        case optcode of
-          "acc" -> aux (pc + 1)
-          "jmp" ->
-            let (r, v) = handheldSimSub [(pc, ("nop", arg))] mem
-             in if r == 0 then v else aux (pc + 1)
-          "nop" ->
-            let (r, v) = handheldSimSub [(pc, ("jmp", arg))] mem
-             in if r == 0 then v else aux (pc + 1)
-        where
-          (optcode, arg) = mem ! pc
-   in aux 0
+  let replace pc ("acc", _)   = replace (pc + 1) (mem ! (pc+1))
+      replace pc ("jmp", arg) = handle pc $ handheldSimSub [(pc, ("nop", arg))] mem
+      replace pc ("nop", arg) = handle pc $ handheldSimSub [(pc, ("jmp", arg))] mem
+      handle pc (end, val) = if end then val else replace (pc + 1) (mem ! (pc+1))
+   in replace 0 (mem ! 0)
