34b4b551a426dc462ddfa3bc691a683d066c5e2b
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-08 15:11:40 +0100

    refactoring day 1

--- day_01.hs
+++ day_01.hs
@@ -1,41 +1,9 @@
--- {-# LANGUAGE TupleSections #-}
-
 main = do
   input <- readFile "day_01.in"
   let input' = map read $ lines input :: [Int]
-  -- print $ naloga1 $ explode1 input'
-  -- print $ naloga2 $ explode2 input'
-  writeFile "day_01_1.out" $ show $ naloga1 $ explode2 input'
-  writeFile "day_01_2.out" $ show $ naloga2 $ explode3 input'
+  writeFile "day_01_1.out" $ show $ naloga1 input'
+  writeFile "day_01_2.out" $ show $ naloga2 input'
 
-find :: (a -> Bool) -> [a] -> a
-find p = head . filter p
+naloga1 xs = head [a * b | a <- xs, b <- xs, a >= b, a + b == 2020]
 
--- naloga1 :: [(Int, Int)] -> Int
-naloga1 = uncurry (*) . find (\(a, b) -> a + b == 2020)
-
--- naloga2 :: [(Int, Int, Int)] -> Int
-naloga2 = (\(a, b, c) -> a * b * c) . find (\(a, b, c) -> a + b + c == 2020)
-
--- naloga1 [] = undefined
--- naloga1 ((a, b) : xs) =
---   if a + b == 2020
---     then a * b
---     else naloga1 xs
-
--- naloga2 :: [(Int, Int, Int)] -> Int
--- naloga2 [] = undefined
--- naloga2 ((a, b, c) : xs) =
---   if a + b + c == 2020
---     then a * b * c
---     else naloga2 xs
-
-explode2 :: [Int] -> [(Int, Int)]
-explode2 xs = [(a, b) | a <- xs, b <- xs, a >= b]
-
--- concatMap (\x -> map (x,) list) list
-
-explode3 :: [Int] -> [(Int, Int, Int)]
-explode3 xs = [(a, b, c) | a <- xs, b <- xs, a >= b, c <- xs, b >= c]
-
--- concatMap (\x -> concatMap (\y -> map (x,y,) list) list) list
+naloga2 xs = head [a * b * c | a <- xs, b <- xs, a >= b, c <- xs, b >= c, a + b + c == 2020]
