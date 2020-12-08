7c7383806597530300fde79a2f90079b6470f567
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-08 15:11:53 +0100

    formatting day 7

--- day_07.hs
+++ day_07.hs
@@ -1,13 +1,11 @@
-import           Control.Applicative          (Alternative ((<|>)))
-import           Data.Char                    (digitToInt, isAlpha, isDigit,
-                                               isSpace)
-import qualified Data.Map.Strict              as PlainMap (fromList)
-import           Data.MultiMap                (MultiMap)
-import qualified Data.MultiMap                as Map (fromList, fromMap, lookup,
-                                                      toList)
-import qualified Data.Set                     as Set (empty, insert)
-import           Text.ParserCombinators.ReadP (eof, many1, option, readP_to_S,
-                                               satisfy, string)
+import Control.Applicative (Alternative ((<|>)))
+import qualified Data.Bifunctor
+import Data.Char (digitToInt, isAlpha, isDigit, isSpace)
+import qualified Data.Map.Strict as PlainMap (fromList)
+import Data.MultiMap (MultiMap)
+import qualified Data.MultiMap as Map (fromList, fromMap, lookup, toList)
+import qualified Data.Set as Set (empty, insert)
+import Text.ParserCombinators.ReadP (eof, many1, option, readP_to_S, satisfy, string)
 
 main = do
   input <- readFile "day_07.in"
@@ -45,26 +43,24 @@ rule = do
 
 parseRule = fst . head . readP_to_S rule
 
--- buildRulesBackwards = foldl (\map (parent, children) -> foldl (\m c -> Map.insert (snd c) parent m) map children) Map.empty
-buildRulesBackwards :: [(String, [(a, String)])] -> MultiMap String String
+buildRulesBackwards :: [(String, [(Int, String)])] -> MultiMap String String
 buildRulesBackwards = Map.fromList . map (\(k, v) -> (snd v, k)) . Map.toList . buildRulesForwards
 
 countParents :: MultiMap String String -> Int
 countParents m =
-  let aux visited []     = visited
-      aux visited (x:xs) = aux (Set.insert x visited) (xs ++ Map.lookup x m)
+  let aux visited [] = visited
+      aux visited (x : xs) = aux (Set.insert x visited) (xs ++ Map.lookup x m)
    in length $ aux Set.empty $ Map.lookup "shiny gold" m
 
 naloga1 = countParents . buildRulesBackwards . map parseRule
 
--- buildRulesForwards = Map.fromMap . foldl (\m (parent, children) -> PlainMap.insert parent children m) PlainMap.empty
 buildRulesForwards :: [([Char], [a])] -> MultiMap [Char] a
 buildRulesForwards = Map.fromMap . PlainMap.fromList
 
 countChildren :: MultiMap [Char] (Int, [Char]) -> Int
 countChildren m =
   let aux k [] = k
-      aux k ((n, x):xs) = aux (k + n) (xs ++ concat (replicate n (Map.lookup x m)))
+      aux k ((n, x) : xs) = aux (k + n) (xs ++ map (Data.Bifunctor.first (n *)) (Map.lookup x m))
    in aux 0 $ Map.lookup "shiny gold" m
 
 naloga2 = countChildren . buildRulesForwards . map parseRule
