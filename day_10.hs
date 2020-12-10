d2c27d3e900a28b9e54e6fa60aac539f5d702f6d
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-10 20:44:07 +0100

    refactored day 10

--- day_10.hs
+++ day_10.hs
@@ -3,6 +3,7 @@ main = do
   input <- readFile "day_10.in"
   let input' = map read $ lines input :: [Int]
   let input'' = sort (0:(maximum input' + 3 ) : input')
+  -- print $ blockLengths input''
   writeFile "day_10_1.out" $ show $ naloga1 input''
   writeFile "day_10_2.out" $ show $ naloga2 input''
 
@@ -14,24 +15,19 @@ naloga1 =
           | otherwise     = aux diff1 diff3 y ys
    in aux 0 0 0
 
-takeBlock :: (Eq a, Num a) => [a] -> [a]
-takeBlock [] = []
-takeBlock [x] = [x]
-takeBlock (x1 : xs@(x2 : _))
-  | x2 - x1 == 1 = x1 : takeBlock xs
-  | otherwise    = [x1]
-
-isValidAdapterSequence :: Int -> [Int] -> Bool
-isValidAdapterSequence _ []       = True
-isValidAdapterSequence n (x : xs) = (x - n <= 3) && isValidAdapterSequence x xs
-
 blockArrangements :: Int -> Int
 blockArrangements n = [1, 1, 1, 2, 4, 7, 13, 24] !! n
-  -- length $ filter (isValidAdapterSequence 1) $ map (\xs -> 1 : xs ++ [n]) $ subsequences [2..n-1]
 
-naloga2 =
-  let aux acc [] = acc
-      aux acc xs =
-        let n = length $ takeBlock xs
-         in aux (acc * blockArrangements n) (drop n xs)
-   in aux 1
+-- reversed
+blockLengths :: [Int] -> [Int]
+blockLengths xs =
+  let aux acc lens (x1 : xs@(x2 : _))
+        | x2 - x1 == 1 = aux (1+acc)        lens  xs
+        | otherwise    = aux      1  (acc : lens) xs
+      aux acc lens _ = acc : lens
+   in aux 1 [] xs
+
+naloga2 xs =
+  let aux acc []     = acc
+      aux acc (n:ns) = aux (acc * blockArrangements n) ns
+   in aux 1 $ blockLengths xs
