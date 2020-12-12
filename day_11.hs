f31c0480d0c620e3f39688baf54515fb633b8180
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-12 02:31:54 +0100

    refactored 11

--- day_11.hs
+++ day_11.hs
@@ -1,50 +1,43 @@
 import           Data.Maybe (mapMaybe)
-data Seat = Null | Empty | Full
-  deriving Eq
+data Seat = Null | Empty | Full deriving (Eq)
+
+instance Read Seat where
+  readList []      = [([], "")]
+  readList (c : k) = [((read [c] :: Seat) : p, tl) | (p, tl) <- readList k]
+  readsPrec _ ('.' : k) = [(Null, k)]
+  readsPrec _ ('L' : k) = [(Empty, k)]
+  readsPrec _ ('#' : k) = [(Full, k)]
+
+instance Show Seat where
+  showList []          s = s
+  showList (c : tl) s    = show c ++ showList tl s
+  showsPrec _ Null  s = '.' : s
+  showsPrec _ Empty s = 'L' : s
+  showsPrec _ Full  s = '#' : s
 
 main = do
   input <- readFile "day_11.in"
-  let input' = map (map toSeat) $ lines input :: [[Seat]]
-  -- print $ fullNeighbours' input' 3 4
-  -- print $ fullNeighbours' input' 1 1
-  -- print $ fullNeighbours' input' 3 3
-  -- print $ (map . map) (\(i, j, x) -> (i, j, fromSeat x)) $ enumerate $ step input'
-  -- putStrLn . unlines $ (map . map) fromSeat input'
-  -- putStrLn . unlines $ (map . map) fromSeat $ step input'
-  -- putStrLn . unlines $ (map . map) fromSeat $ step $ step input'
-  -- putStrLn . unlines $ (map . map) fromSeat $ fix step input'
+  -- print $ show (read "L" :: Seat)
+  -- print $ show (read ".." :: [Seat])
+  -- putStrLn . unlines $ map show $ fix step input'
+  let input' = map read $ lines input :: [[Seat]]
   writeFile "day_11_1.out" $ show $ naloga1 input'
   writeFile "day_11_2.out" $ show $ naloga2 input'
 
-toSeat :: Char -> Seat
-toSeat '.' = Null
-toSeat 'L' = Empty
-toSeat '#' = Full
+(!!!) :: [a] -> Int -> Maybe a
+(!!!) [] _       = Nothing
+(!!!) (x : _) 0  = Just x
+(!!!) (_ : xs) n = xs !!! (n-1)
 
-fromSeat :: Seat -> Char
-fromSeat Null  = '.'
-fromSeat Empty = 'L'
-fromSeat Full  = '#'
+cardinalDirs = [ (-1, -1), ( 0, -1), ( 1, -1), (-1,  0), ( 1,  0), (-1,  1), ( 0,  1), ( 1,  1) ]
 
 getStatus :: [[Seat]] -> Int -> Int -> Maybe Seat
-getStatus grid x y =
-  if x < 0 || y < 0 || x >= length (head grid) || y >= length grid
-    then Nothing
-    else Just (grid !! y !! x)
+getStatus grid x y = grid !!! y >>= (!!! x)
 
 neighbours :: [[Seat]] -> Int -> Int -> [Seat]
 neighbours grid x y = mapMaybe (uncurry $ getStatus grid) neighbourIndexes
   where
-    neighbourIndexes = [
-        (x-1, y-1),
-        (x  , y-1),
-        (x+1, y-1),
-        (x-1, y  ),
-        (x+1, y  ),
-        (x-1, y+1),
-        (x  , y+1),
-        (x+1, y+1)
-      ]
+    neighbourIndexes = map (\(di, dj) -> (x+di,y+dj)) cardinalDirs
 
 fullNeighbours :: [[Seat]] -> Int -> Int -> Int
 fullNeighbours grid x y = length $ filter (== Full) $ neighbours grid x y
@@ -55,15 +48,15 @@ nextValue grid x y Empty = if fullNeighbours grid x y == 0 then Full else Empty
 nextValue _ _ _    Null  = Null
 
 step :: [[Seat]] -> [[Seat]]
-step grid =
-  [[nextValue grid i j x | (i, x)   <- zip [0..] row]
-                         | (j, row) <- zip [0..] grid]
+step grid = [[nextValue grid i j x | (i, x) <- zip [0..] row]
+            | (j, row) <- zip [0..] grid]
 
 fix :: Eq t => (t -> t) -> t -> t
 fix f x = let x' = f x in if x == x' then x else fix f x'
 
 naloga1 = length . filter (== Full) . concat . fix step
 
+fullInDirection :: [[Seat]] -> Int -> Int -> (Int, Int) -> Int
 fullInDirection grid x y (dx, dy) =
   let aux grid x y (dx, dy) k =
         case getStatus grid (x+k*dx) (y+k*dy) of
@@ -73,20 +66,10 @@ fullInDirection grid x y (dx, dy) =
           Nothing    -> 0
    in aux grid x y (dx, dy) 1
 
-
 fullNeighbours' :: [[Seat]] -> Int -> Int -> Int
 fullNeighbours' grid x y = sum neighbourIndexes
   where
-    neighbourIndexes = [
-        fullInDirection grid x y (-1, -1),
-        fullInDirection grid x y ( 0, -1),
-        fullInDirection grid x y ( 1, -1),
-        fullInDirection grid x y (-1,  0),
-        fullInDirection grid x y ( 1,  0),
-        fullInDirection grid x y (-1,  1),
-        fullInDirection grid x y ( 0,  1),
-        fullInDirection grid x y ( 1,  1)
-      ]
+    neighbourIndexes = map (fullInDirection grid x y) cardinalDirs
 
 nextValue' :: [[Seat]] -> Int -> Int -> Seat -> Seat
 nextValue' grid x y Full  = if fullNeighbours' grid x y >= 5 then Empty else Full
@@ -94,8 +77,7 @@ nextValue' grid x y Empty = if fullNeighbours' grid x y == 0 then Full else Empt
 nextValue' _ _ _    Null  = Null
 
 step' :: [[Seat]] -> [[Seat]]
-step' grid =
-  [[nextValue' grid i j x | (i, x)   <- zip [0..] row]
-                          | (j, row) <- zip [0..] grid]
+step' grid = [[nextValue' grid i j x | (i, x) <- zip [0..] row]
+             | (j, row) <- zip [0..] grid]
 
 naloga2 = length . filter (== Full) . concat . fix step'
