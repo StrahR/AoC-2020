2fff1a5af9ee38589d5c35a660a1e9caa27902b0
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-16 10:33:17 +0100

    refactored 15

--- day_15.hs
+++ day_15.hs
@@ -1,5 +1,4 @@
-import           Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as Map (empty, insert, lookup)
+import qualified Data.IntMap.Lazy as Map (fromList, insert, lookup)
 import           Data.List.Split
 
 main = do
@@ -8,15 +7,14 @@ main = do
   writeFile "day_15_1.out" $ show $ naloga1 input'
   writeFile "day_15_2.out" $ show $ naloga2 input'
 
+findnth :: Int -> [Int] -> Int
 findnth stop input =
-  let initMap acc _ []       = acc
-      initMap acc n (x : xs) = initMap (Map.insert x n acc) (n+1) xs
-      aux _ n k | n == stop - 1  = k
+  let aux _ n k | n == stop - 1  = k
       aux map n k  =
         case Map.lookup k map of
           Just m  -> aux (Map.insert k n map) (n+1) (n - m)
           Nothing -> aux (Map.insert k n map) (n+1) 0
-   in aux (initMap Map.empty 0 input) (length input) 0
+   in aux (Map.fromList $ zip input [0..]) (length input) 0
 
 naloga1 = findnth 2020
 
