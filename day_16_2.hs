8aa6b8fd1973e934bcf9fdc5c3ec6e22f87f44f3
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-12-16 12:34:49 +0100

    day 16

---
+++ day_16_2.hs
@@ -0,0 +1,42 @@
+n =
+  [ [ ("arrival station", 13)
+    ],
+    [ ("arrival track", 18)
+    ],
+    [ ("class", 7)
+    ],
+    [ ("row", 10)
+    ],
+    [ ("route", 4)
+    ],
+    [ ("type", 16)
+    ],
+    [ ("arrival platform", 2)
+    ],
+    [ ("departure date", 11)
+    ],
+    [ ("departure platform", 14)
+    ],
+    [ ("departure location", 9)
+    ],
+    [ ("departure time", 19)
+    ],
+    [ ("departure station", 6)
+    ],
+    [ ("departure track", 17)
+    ],
+    [ ("seat", 15)
+    ],
+    [ ("price", 8)
+    ],
+    [ ("arrival location", 5)
+    ],
+    [ ("zone", 12)
+    ],
+    [ ("duration", 3)
+    ],
+    [ ("train", 1)
+    ],
+    [ ("wagon", 20)
+    ]
+  ]
